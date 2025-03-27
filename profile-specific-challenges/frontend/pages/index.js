4ffd17fc0ffa647bba98871965bb67881e7fe479
LucaPalminteri <lucapalminteri02@gmail.com>
2025-03-27 14:22:29 -0300

    feat: add status challenges

--- profile-specific-challenges/frontend/pages/index.js
+++ profile-specific-challenges/frontend/pages/index.js
@@ -1,28 +1,42 @@
 import Link from "next/link";
 
 const paths = [
+  "autocomplete",
   "file-tree-viewer",
   "hooks",
   "market",
+  "shopping-cart",
   "signup",
+  "stopwatch",
   "sudoku",
   "tetris",
   "questionnaire",
-  "autocomplete",
-  "stopwatch",
-  "weather"
+  "weather",
 ];
+
+const completedChallenges = new Set(["signup"]);
+const ongoingChallenges = new Set(["tetris"]);
 
 export default function Home() {
   return (
     <div style={{ padding: "20px", margin: "20px" }}>
-      <h1> React Challenges </h1>
+      <h1>React Challenges</h1>
       <ol>
-        {paths.map((path, i) => (
-          <li key={i} style={{ margin: "10px" }}>
-            <Link href={`/challenges/${path}`}>{path}</Link>
-          </li>
-        ))}
+        {paths.map((path, i) => {
+          let color = "red";
+          if (completedChallenges.has(path)) {
+            color = "green";
+          } else if (ongoingChallenges.has(path)) {
+            color = "orange";
+          }
+          return (
+            <li key={i} style={{ margin: "10px" }}>
+              <Link href={`/challenges/${path}`} style={{ color }}>
+                {path}
+              </Link>
+            </li>
+          );
+        })}
       </ol>
     </div>
   );
