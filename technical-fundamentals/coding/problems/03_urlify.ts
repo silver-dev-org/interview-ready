736eb3b7efb9458ffff8b741d23240fb19c92ddf
JuaniSilva <juanicruzsilva@gmail.com>
2025-08-26 19:30:31 -0300

    Revert "fix: space"
    This reverts commit 401eee2041f0678f781149cb3eb9533be0975181.

--- technical-fundamentals/coding/problems/03_urlify.ts
+++ technical-fundamentals/coding/problems/03_urlify.ts
@@ -8,7 +8,7 @@ export default function URLify(s1: string): string {
 	let newString = '';
 	for (let i = 0; i < s1.length; i++) {
 		let char = s1[i];
-		if (char === ' ') {
+		if (char == '') {
 			char = '%20';
 		}
 		newString = `${newString}${char}`;
