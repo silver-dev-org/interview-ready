2fe5fab608fbe0c11387aab8b5918151df422159
Gabriel Benmergui <gabriel.benmergui@gmail.com>
2025-08-26 09:18:17 -0300

    add a n easy group by challenge

---
+++ technical-fundamentals/coding/groupBy.mjs
@@ -0,0 +1,11 @@
+/*
+  We have a set of 100 clients that have purchased various amount of products in our service.
+  Our analytics platform is not suited for it so we want to build a short script that can always answer a few questions for us:
+  1. What is the distribution of clients based on the # of orders? (How many purchased 1, 2, 3, etc)
+  2. What is the mean volume(in $) of our clients total value?
+
+  The raw data is a json file at:
+  https://gist.githubusercontent.com/conanbatt/9da0470db6cf90c61324f8365d448a92/raw/caa9c9e8d365abd374b5d2e1ef0f380b75d45962/.json
+*/
+
+export class ClientAnalytics {}
